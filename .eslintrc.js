95174b3a050c0861c64d9a4fb339b8fee0e43860
Yurick <yurick.hauschild@gmail.com>
2023-03-05 22:04:52 +0100

    🎉 Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+  extends: ['prettier'],
+}
