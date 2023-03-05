f14a85cb0e560d55727a6cc121ff636f2f8355fa
Yurick <yurick.hauschild@gmail.com>
2023-03-05 22:21:22 +0100

    ✨  Add react-native-navigation

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import { AppRegistry } from 'react-native'
-import App from './App'
-import { name as appName } from './app.json'
+import { Navigation } from 'react-native-navigation'
+import App from './routes/App'
 
-AppRegistry.registerComponent(appName, () => App)
+Navigation.registerComponent('Home', () => App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [{ component: { name: 'Home' } }],
+      },
+    },
+  })
+})
